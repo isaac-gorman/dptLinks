354f4c02152a6a3743318d10f0291dbe5ce78017
Isaac Gorman <emailme@isaacg.net>
2020-09-01 15:07:48 -0500

    LinkData is displaying

---
+++ prj/src/components/Data/StudyLinksData.js
@@ -0,0 +1,27 @@
+import LP from "./LP.svg";
+
+const StudyLinksData = [
+  { id: 0, title: "Lab Videos", image: LP, destination: "Youtube" },
+  { id: 1, title: "Lab Photos", image: LP, destination: "Google Drive" },
+  {
+    id: 2,
+    title: "School Calander",
+    image: LP,
+    destination: "Google Calander",
+  },
+  {
+    id: 3,
+    title: "Student Made Material",
+    image: LP,
+    destination: "Google Drive",
+  },
+  { id: 4, title: "Textbooks", image: LP, destination: "Google Drive" },
+  {
+    id: 5,
+    title: "Lecture Recordings",
+    image: LP,
+    destination: "Google Drive",
+  },
+];
+
+export default StudyLinksData;
--- prj/src/components/StudyLinks.js
+++ prj/src/components/StudyLinks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import StudyLinksData from "./Data/StudyLinksData";
 
 const Container = styled.div`
   width: 100%;
@@ -7,11 +8,9 @@ const Container = styled.div`
 
   // background: purple;
 `;
-
 const Heading = styled.h3`
   font-size: 16px;
 `;
-
 const CarouselConatiner = styled.div`
   width: 100%;
   height: 90%;
@@ -20,7 +19,6 @@ const CarouselConatiner = styled.div`
   white-space: nowrap;
   margin: 0 auto;
 `;
-
 const Track = styled.div`
   width: 100%;
   height: 100%;
@@ -34,38 +32,91 @@ const Track = styled.div`
     width: 0px;
   }
 `;
-
 const LinksDivA = styled.div`
   width: 400px;
-  height: 80%;
+  height: 100%;
   flex-shrink: 0;
 
   background: yellow;
 `;
 const LinksDivB = styled.div`
   width: 400px;
-  height: 80%;
+  height: 100%;
   flex-shrink: 0;
 
   background: orange;
 `;
 
-// const LinksB = styled.div`
-//   width: 400px;
-//   height: 80%;
+const LinkCard = styled.div`
+  width: 240px;
+  height: 55px;
+  background: grey;
+  display: flex;
+  // justify-content: center;
+  align-items: center;
+`;
 
-//   background: orange;
-// `;
+const Icon = styled.img`
+  width: 50px;
+  height: 50px;
+  background: pink;
+  margin-left: 6px;
+`;
 
+const SubjectDiv = styled.div`
+  width: 70%;
+  height: 50px;
+  background: red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 12px;
+`;
+
+const Title = styled.h4`
+  font-size: 14px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const Destination = styled.p`
+  font-size: 12px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
 function StudyLinks() {
   return (
     <Container>
       <Heading>Study Links</Heading>
       <CarouselConatiner>
         <Track>
-          <LinksDivA></LinksDivA>
-          <LinksDivB></LinksDivB>
-          {/* <LinksB></LinksB> */}
+          <LinksDivA>
+            {/* .slice(0, 3) */}
+            {StudyLinksData.slice(0, 3).map((item) => {
+              return (
+                <LinkCard key={item.id}>
+                  <Icon src={item.image} alt="item" />
+                  <SubjectDiv>
+                    <Title>{item.title}</Title>
+                    <Destination>{item.destination}</Destination>
+                  </SubjectDiv>
+                </LinkCard>
+              );
+            })}
+          </LinksDivA>
+          <LinksDivB>
+            {StudyLinksData.slice(3).map((item) => {
+              return (
+                <LinkCard key={item.id}>
+                  <Icon src={item.image} alt="item" />
+                  <SubjectDiv>
+                    <Title>{item.title}</Title>
+                    <Destination>{item.destination}</Destination>
+                  </SubjectDiv>
+                </LinkCard>
+              );
+            })}
+          </LinksDivB>
         </Track>
       </CarouselConatiner>
     </Container>
